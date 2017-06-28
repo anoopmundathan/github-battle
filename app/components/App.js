357e5ee04b3a5a7b642c7741925819fc24f206cd
anoopmundathan <anoop.mundathan@gmail.com>
2017-06-28 09:09:17 +0100

    Add react router

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route, 
+    Link
+} from 'react-router-dom';
+
 import Popular from './Popular';
+
+const Home = () => <div>Home component</div>
+const Battle = () => <div>Battle component</div>
 
 class App extends Component {
     render() {
         return(
-            <div className='container'>
-                <Popular />
-            </div>
+               <Router>
+                   <div>
+                        <ul className='nav-container'>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/battle'>Battle</Link></li>
+                            <li><Link to='/popular'>Popular</Link></li>
+                        </ul>
+                        <div className='container'>
+                            <Route path='/popular' component={Popular}/>
+                            <Route exact path='/' component={Home}/>
+                            <Route path='/battle' component={Battle}/>
+                        </div>
+                    </div>
+                </Router>
         );
     }
 }
