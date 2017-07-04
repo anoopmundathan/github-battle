a78f90c76218201c27293e9a273d43a7a67caf7a
anoopmundathan <anoop.mundathan@gmail.com>
2017-07-04 08:41:15 +0100

    Battle player and decide winner

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,38 +1,51 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
+import { battle } from '../utl/api';
 
-const parse = (str) => {
-
-    if(typeof str !== 'string') {
-        return {}
-    }
-
-    str = str.trim().replace(/^\?/, '');
-
-    if(!str) {
-        return {}
-    }
-
-    return str.trim().split('&').reduce((ret, param) => {
-        var parts = param.split('=');
-        ret[parts[0]] = parts[1] === undefined ? null : decodeURIComponent(parts[1]);
-        return ret;
-    }, {});
-    
-}
 
 class Results extends Component {
     
-    render() {
-        
-        const parsed = parse(this.props.location.search);
-        console.log(parsed);
+    constructor() {
+        super();
+        this.state ={
+            winner: null,
+            looser: null,
+            error: null,
+            loading: true
+        }
+    }
+    componentDidMount() {
+        const parsed = queryString.parse(this.props.location.search);
+        battle([
+            parsed.playerOneName, 
+            parsed.playerTwoName
+        ]).then(results => {
+            this.setState({
+                winner: results[0].profile.name,
+                looser: results[1].profile.name,
+                loading: false
+            })
+        });
+    }
 
-        return(
-            <div>
-                Result Component
-            </div>
-        );
+    render() {     
+        const error = this.state.error;
+        const winner = this.state.winner;
+        const looser = this.state.looser;
+        const loading = this.state.loading;
+
+        if (loading === true) {
+            return(
+                <p>Loading...</p>
+            );
+        } else {
+            return(
+                <div>
+                    <h1>Winner: {this.state.winner}</h1>
+                    <h1>Looser: {this.state.looser}</h1>
+                </div>
+            );
+        }   
     }
 }
 
--- app/utl/api.js
+++ app/utl/api.js
@@ -1,5 +1,59 @@
+import axios from 'axios';
+
+const id = 'fcb676561ad5ae9f63de';
+const sec = '0532513f386e8a73719caf2b656e026e5018bd5d';
+const params = `?client_id=${id}&client_secret=${sec}`;
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getStarCount(repos) {
+    return repos.data.reduce((count, repo) => {
+        return count + repo.stargazers_count; 
+    }, 0)
+}
+
+function calculateScore(profile, repos) {
+    const followers = profile.followers;
+    const totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function getProfile(username) {
+    return axios.get(`https://api.github.com/users/${username}${params}`)
+            .then(user => user.data);
+}
+
+function getRepos(username) {
+    return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`);
+}   
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(data => {
+        const profile = data[0];
+        const repos = data[1];
+
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        }
+    });
+}
+
+function sortPlayers(players) {
+    return players.sort((a, b) => b.score - a.score);
+}
+
 function battle(players) {
-    console.log(players);
+    return axios.all(players.map(getUserData))
+        .then(sortPlayers)
+        .catch(handleError);
 }
 
 function fetchRepo(language) {
